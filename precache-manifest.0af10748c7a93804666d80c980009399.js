a70c4e35f3f82e6b594dbdc46811165f0cbb85e4
SamMitchell153 <Sam.Mitchell153@gmail.com>
2019-11-14 12:07:51 +1100

    Updates

---
+++ precache-manifest.0af10748c7a93804666d80c980009399.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a26f38bf3682aef263b5072ed8f5ed40",
+    "url": "/test-github-io/index.html"
+  },
+  {
+    "revision": "11fa33d18e7355c52dc5",
+    "url": "/test-github-io/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "597cbb0c7a0a23f77dee",
+    "url": "/test-github-io/static/js/2.92dcdfb0.chunk.js"
+  },
+  {
+    "revision": "11fa33d18e7355c52dc5",
+    "url": "/test-github-io/static/js/main.1bb2f044.chunk.js"
+  },
+  {
+    "revision": "1ba17c366cf43aa04db5",
+    "url": "/test-github-io/static/js/runtime-main.67dbf27b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/test-github-io/static/media/logo.25bf045c.svg"
+  }
+]);
